7c9b7470deace836991e2764fd70ce32619dfa65
istar <istar@192.168.100.8>
2017-05-25 09:37:19 +0530

    updated by swetha88 arithematic oprtr

---
+++ src/com/onlinecalculator/calculator/calctypes/artmtcoptr.java
@@ -0,0 +1,26 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class artmtcoptr {
+	public static void main(String args[])
+	{
+		double a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first num");
+		a=sc.nextDouble();
+		System.out.println("enter secnd num");
+		b=sc.nextDouble();
+		System.out.println("1.addition"+(a+b));
+		System.out.println("2.substraction"+(a-b));
+		System.out.println("3.multiplication"+(a*b));
+		System.out.println("4.division"+(a/b));
+		
+
+		
+		
+	}
+
+
+}
+
+
+
