a83adc809ef0ec40203b7e290378197e4db2a04d
istar <istar@192.168.100.8>
2017-05-25 09:18:08 +0530

    assignment oprtr updated by swetha88

---
+++ src/com/onlinecalculator/calculator/calctypes/assignmntoprtr.java
@@ -0,0 +1,31 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class assignmntoprtr {
+	public static void main(String args[])
+	{
+		double a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first num");
+		a=sc.nextDouble();
+		System.out.println("enter secnd num");
+		b=sc.nextDouble();
+		System.out.println("a=b"+(a=b));
+		System.out.println("a+=b"+(a=a+b));
+		System.out.println("a-=b"+(a=a-b));
+		System.out.println("a*=b"+(a=a*b));
+		System.out.println("a/=b"+(a=a/b));
+		System.out.println("a%=b"+(a=a%b));
+		
+
+		
+		
+	}
+	
+
+}
+
+
+
+
