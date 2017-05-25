b52ac02c41e81c539467affc70f2b0893cbc6f79
istar <istar@192.168.100.8>
2017-05-25 18:35:03 +0530

    updatd by swetha

---
+++ src/com/onlinecalculator/calculator/calctypes/mno.java
@@ -0,0 +1,28 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+class instance
+{
+	int x;
+   void display()
+	{
+		x=15;
+		Scanner s=new Scanner(System.in);
+		System.out.println("instance variablex="+x);
+	}
+}
+		
+public class mno {
+	public static void main(String args[])
+	{
+		instance s=new instance();
+		s.display();
+		
+	}
+	
+		
+
+
+	}
+
+
+
