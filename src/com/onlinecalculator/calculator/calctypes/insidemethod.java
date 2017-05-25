4504b749c7292cb12f4be5de22e6ca22bee3a649
istar <istar@192.168.100.8>
2017-05-25 17:49:49 +0530

    updated by swetha local variable

---
+++ src/com/onlinecalculator/calculator/calctypes/insidemethod.java
@@ -0,0 +1,18 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class insidemethod {
+	static void display()
+	{
+		int x=10;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("local variablex="+x);
+	}
+		public static void main(String args[])
+		{
+			display();
+	}
+		
+}
+
+
+
