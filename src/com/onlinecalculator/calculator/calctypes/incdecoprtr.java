1c4f1ef2538310a2a60b40e41c1eef9e41cd91f7
istar <istar@192.168.100.8>
2017-05-25 09:16:10 +0530

    increment decrement oprtr updated by swetha88

---
+++ src/com/onlinecalculator/calculator/calctypes/incdecoprtr.java
@@ -0,0 +1,28 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class incdecoprtr {
+	public static void main(String args[])
+	{
+		double a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first num");
+		a=sc.nextDouble();
+		System.out.println("++a"+(++a));
+		System.out.println("--a"+(--a));
+		System.out.println("a"+(a));
+		
+		
+		
+
+		
+		
+	}
+	
+
+}
+
+
+
+
