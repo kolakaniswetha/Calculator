28a4ae5359f8d1e3f9988fcd286206d2b3a458ed
HOME <HOME@192.168.0.8>
2017-06-03 11:11:10 -0700

    mainprogram code by swetha

--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -1,31 +1,51 @@
 package com.onlinecalculator.calculator.maincalc;
-
 import java.util.Scanner;
 
-import com.onlinecalculator.calculator.calctypes.*;
-
+import com.onlinecalculator.calculator.calctypes.BasicCalc;
+import com.onlinecalculator.calculator.calctypes.ScientificCalc;
+import com.onlinecalculator.calculator.calctypes.StatisticCalc;
 public class MainPgm {
-public static void main(String args[]){
-        char type;
-        type=getType();
-        if(type=='a')
+public static void main(String args[])
+{
+        int type;
+        type=disp();
+        if(type==1)
         {
-        	
+         BasicCalc cal=new  BasicCalc();
+   		 cal.menu();
+   		 cal.userInputs();
+   		 cal.performOperation();
         }
-        if(type=='b')
+        if(type==2)
         {
+        	ScientificCalc scal =new ScientificCalc();
+      		scal.menu();
+      		scal.userInputs();
+      		scal.performOperation();
        		
        	}
         	
-        	if(type=='c')
+        	if(type==3)
         	{
-        		
+        		StatisticCalc scal=new StatisticCalc();
+        		scal.menu();
+         		scal.userInputs();
+         		scal.performOperation();
         	}
- 
-    }
+}
+
         
-    static char getType()
+    static int disp()
     {
-    	
+    	int ch;
+    	Scanner s=new Scanner(System.in);
+    	System.out.println("************************Calculator*************************************");
+    	System.out.println("************************1.Basic Calculator*************************************");
+    	System.out.println("************************2.Scientific Calculator*************************************");
+    	System.out.println("***********************3.Statistical Calculator*************************************");
+    	System.out.println("*************************************************************");
+    	System.out.println("please enter any option");
+    	ch=s.nextInt();
+    	return(ch);
     }
 }
